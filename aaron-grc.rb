176525af2779f66941f660a71777887ae21c5cb2
Aaron Greenberg <a@aaronjgreenberg.com>
2013-04-28 16:50:45 -0400

    Bump version

--- aaron-grc.rb
+++ aaron-grc.rb
@@ -7,7 +7,7 @@ class AaronGrc < Formula
   homepage 'http://korpus.juls.savba.sk/~garabik/software/grc.html'
   url 'https://github.com/aaronjgreenberg/grc/archive/master.zip'
   sha1 '4cb665f29031981c0d0c03a1b33115239acdfa2f'
-  version '1'
+  version '2'
 
   def install
     inreplace ['bin/grc', 'share/man/man1/grc.1'], '/etc', etc
